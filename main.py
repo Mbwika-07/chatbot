1289aeab3ee3d946378be41dff0e03b8c23e4edf
Mbwika-07 <allanmbwiks@gmail.com>
2024-02-01 20:51:27 +0300

    Issues fixed

--- main.py
+++ main.py
@@ -16,12 +16,10 @@ def find_best_match(user_question: str, questions: list[str]) -> str | None:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
     return matches[0] if matches else None
 
-
 def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
     for q in knowledge_base["questions"]:
         if q["question"] == question:
             return q["answer"]
-
 
 def chat_bot():
     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
