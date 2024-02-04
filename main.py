efe03ad3f4e74a46952e3f215462197d59c67008
Mbwika-07 <allanmbwiks@gmail.com>
2024-02-04 19:58:57 +0300

    .

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
         if q["question"] == question:
             return q["answer"]
 
-
 def chat_bot():
     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
     
