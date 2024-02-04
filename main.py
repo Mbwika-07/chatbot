c727681bff37ab18dee6edc4b0dc6be2d1f358d1
Mbwika-07 <allanmbwiks@gmail.com>
2024-02-04 20:03:28 +0300

    .

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def load_knowledge_base(file_path: str) -> dict:
     return data
 
 
-
 def save_knowledge_base(file_path: str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
