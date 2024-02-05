7425de6fb2fdc2a4d5d51241a9fc9cb209181baa
Mbwika-07 <allanmbwiks@gmail.com>
2024-02-05 18:39:25 +0300

    .

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ def load_knowledge_base(file_path: str) -> dict:
         data: dict = json.load(file)
     return data
 
-
 def save_knowledge_base(file_path: str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
-
 
 
 def find_best_match(user_question: str, questions: list[str]) -> str | None:
